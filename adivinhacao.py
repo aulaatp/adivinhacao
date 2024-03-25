cc2f159c74c771557d388d06460a56113849b0d3
aulaatp <163074226+aulaatp@users.noreply.github.com>
2024-03-25 20:55:44 -0300

    Primeiro commit

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,14 +1,18 @@
+#biblioteca para usar um número aleatório criado pelo programa
 import random
-
+#comando que cria um número aleatório criado pelo programa entre 1 e 100
 numero_secreto = random.randint(1, 100)
-
+#loop que solicita um número ao usuário enquanto estiver errando
 while True:
+  #solicita um número do usuário
   numero_digitado = int(input("Digite um número entre 1 e 100: "))
-
+  #verifica se o número é igual -> o usuário acertou
   if numero_digitado == numero_secreto:
     print("Parabéns! Você adivinhou o número secreto!")
     break
+  #verifica se o número digitado é maior
   elif numero_digitado > numero_secreto:
     print("O número secreto é menor!")
+  #se nao for igual e nem maior o numero digitado eh menor
   else:
     print("O número secreto é maior!")
